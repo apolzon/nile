3dbc7de228863e62b3a26c536c2ab3ea2f1f9b8f
Chris Apolzon <apolzon@gmail.com>
2011-12-12 22:17:15 -0800

    commit of new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nile::Application.config.secret_token = '76c21cf16fcdee385d0c5a478b3f44a1f3af520b30eb2a2e730eb90875977f430dd62d4c19da2efca8a87986eb31259857ed2b2d188cdde614b1720aa409e52e'
